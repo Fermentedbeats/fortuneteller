9e3a24804f58a4c49cac23bf11836ad0d5a2ceb9
CJ Johnson <Fermentedbeats@gmail.com>
2015-06-07 12:32:40 -0700

    TweenMax animation on colors

--- ft.js
+++ ft.js
@@ -1,25 +1,42 @@
+
 function begin() {
 	var btn = document.getElementById("begin");
-	btn.addEventListener("click", populateColors());
+	btn.addEventListener("click", function() {
+		populateColors();
+	});
+	
 }
 
 function populateColors() {
 	var menu = document.getElementById("menu");
+	var divs = menu.getElementsByTagName("div");
 
-	for (var i = 0; i < 63; i++) {
-		var next = createColorDiv();
-		next.className = "colors";
-		menu.appendChild(next);
+	// set if for first-time run
+	if (divs.length === 0) {
+		for (var i = 0; i < 63; i++) {
+			var next = createColorDiv();
+			next.className = "colors";
+			menu.appendChild(next);
+			TweenLite.from(next, 0.5, {opacity:0, scale:0, ease: Bounce.easeOut});
+		}
+
+		function createColorDiv() {
+			var div = document.createElement("div");
+			div.style.backgroundColor = assignColor();
+			return div;
+		}
+		document.getElementById("begin").value = "Screw this, I want better colors";
+		createHeader();
 	}
-	function createColorDiv() {
-		var div = document.createElement("div");
-		var color = assignColor();
-		div.style.backgroundColor = color;
-		return div;
+	else {
+		for (var k = 0; k < divs.length; k++) {
+			divs[k].style.backgroundColor = assignColor();
+		}
 	}
+
 	function assignColor() {
 		var hex = "#";
-		colorArray = [1,5,9,"f"];
+		colorArray = [1,2,3,4,5,6,7,8,9,"a","b","c","d","e","f"];
 		for (var j=0; j < 6; j++) {
 			var rando = Math.floor(Math.random()*colorArray.length);
 			hex += colorArray[rando];
@@ -28,5 +45,10 @@ function populateColors() {
 	}
 }
 
+function createHeader() {
+
+	var header = document.getElementById("header");
+	header.innerHTML = "Choose A Color";
+}
 
 begin();
