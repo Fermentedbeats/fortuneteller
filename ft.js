ea7653cfe07aa8d1bb9b050028d1b7e2eed4a09f
CJ Johnson <Fermentedbeats@gmail.com>
2015-06-07 14:21:56 -0700

    extract hex #

--- ft.js
+++ ft.js
@@ -1,4 +1,4 @@
-
+// 1. game begins w/ offering  color pallete
 function begin() {
 	var btn = document.getElementById("begin");
 	btn.addEventListener("click", function() {
@@ -10,9 +10,9 @@ function populateColors() {
 	var menu = document.getElementById("menu");
 	var divs = menu.getElementsByTagName("div");
 
-	// set if for first-time run
+	// create colors for first-time run
 	if (divs.length === 0) {
-		for (var i = 0; i < 63; i++) {
+		for (var i = 0; i < 70; i++) {
 			var next = createColorDiv();
 			next.className = "colors";
 			menu.appendChild(next);
@@ -22,13 +22,16 @@ function populateColors() {
 
 		function createColorDiv() {
 			var div = document.createElement("div");
-			div.style.backgroundColor = assignColor();
+			var color = div.style.backgroundColor = assignColor();
+			div.addEventListener("click", function() {
+				findColorNum(color);
+			});
 			return div;
 		}
 		// change button text
 		document.getElementById("begin").value = "Screw this, I want better colors";
 
-		createHeader();
+		createChooseColorHeader();
 	}
 	else {
 		for (var k = 0; k < divs.length; k++) {
@@ -50,12 +53,40 @@ function populateColors() {
 	}
 }
 
-function createHeader() {
-
+function createChooseColorHeader() {
 	var header = document.getElementById("header");
 	header.innerHTML = "Choose A Color";
 	// slide in title from left
 	TweenMax.from(header, 1, {opacity:0, x:-800, ease:Elastic.eastOut}, 0.7);
 }
+
+// 2. color is chosen
+// find num in hex and apply that to card spins + result
+function findColorNum(color) {
+	console.log("COLOR IS " + color)
+	var colorNum;
+	// iterate thru hex chars
+	for (var m = 1; m < color.length; m++) {
+		// if you have a hex letter instead of a num numTF is false
+		var numTF = isNaN(color[m] * 1);
+		//if you do have a num, assign it to the var colorNum
+		if (!numTF) {
+			colorNum = color[m];
+			break;
+		}
+	}
+	// if the hex contained no nums, generate a random num
+	if (colorNum === undefined) {
+		colorNum = Math.floor(Math.random()*6);
+	}
+	flipCard(colorNum);
+}
+
+
+function flipCard(num) {
+	console.log("num is " + num);
+}
+// 3. choose a num
+// apply num to card spins + result
 
 begin();
