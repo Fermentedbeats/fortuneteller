85f57c8b3c2dee6582677571acc5154d4f6ec071
CJ Johnson <Fermentedbeats@gmail.com>
2015-06-08 19:47:47 -0700

    break out rounds

--- ft.js
+++ ft.js
@@ -1,8 +1,10 @@
 
+function fortuneTellerApp() {
 //global vars
 
-var cards = ["WEALTH", "FORTUNE", "HAPPINESS", "SORROW", "LOSS", "SHAME", "REGRETS", "JOY", "HEALTH", "SUCCESS", "MONEY", "CALM", "RESPECT", "FAMILY", "ADVENTURE", "THRILLS", "LOVE", "TIME", "DEEP", "LONGING", "MURDER", "INTRIGUE", "STRANGER", "WISDOM", "CHOICES", "EASY", "HARD"];
+var cards = ["WEALTH", "FORTUNE", "HAPPINESS", "SORROW", "LOSS", "SHAME", "REGRETS", "JOY", "HEALTH", "SUCCESS", "MONEY", "CALM", "RESPECT", "FAMILY", "ADVENTURE", "THRILLS", "LOVE", "TIME", "DEEP", "LONGING", "MYSTERY", "INTRIGUE", "STRANGER", "WISDOM", "CHOICES", "EASY", "HARDSHIP", "PEACE"];
 var imgs = [1,2,3,4,5,6,7,8,9];
+var round = 1;
 
 
 // 1. game begins w/ offering  color pallete
@@ -19,20 +21,26 @@ function populateColors() {
 
 	// create colors for first-time run
 	if (divs.length === 0) {
-		for (var i = 0; i < 10; i++) {
+		for (var i = 0; i < 11; i++) {
 			var next = createColorDiv();
 			next.className = "colors";
 			menu.appendChild(next);
+			// assign shape to divs
+			TweenMax.to(next, .75, {
+ 		   		borderRadius:"0px 20px"
+			});
+
+
 			// drop divs from sky, rotate & bounce
 			var randBounce = Math.floor(Math.random()*1080 - 540)
-			TweenMax.from(next, 1, {opacity:0, scale:0, rotation:randBounce, ease: Elastic.easeOut, y:randBounce, x:randBounce});
+			TweenMax.from(next, i*.3, {opacity:0, scale:0, rotation:randBounce, ease: Elastic.easeOut, y:randBounce, x:randBounce});
 		}
 
 		function createColorDiv() {
 			var div = document.createElement("div");
 			var color = div.style.backgroundColor = assignColor();
 			div.addEventListener("click", function() {
-				findColorNum(color);
+				findColorNum(color, div);
 			});	
 			return div;
 		}
@@ -49,7 +57,8 @@ function populateColors() {
 
 		}
 	}
-
+	// generate random hex color
+}
 	function assignColor() {
 		var hex = "#";
 		colorArray = [1,2,3,4,5,6,7,8,9,"a","b","c","d","e","f"];
@@ -59,24 +68,21 @@ function populateColors() {
 		}
 		return hex;
 	}
-}
-
+// change title on page to ask for color selection
 function createChooseColorHeader() {
 	var header = document.getElementById("header");
 	header.innerHTML = "Choose A Color";
 	// slide in title from left
-	TweenMax.from(header, 1, {opacity:0, x:-800, ease:Elastic.eastOut}, 0.7);
+	TweenMax.from(header, 2, {opacity:0, x:-800, ease:Elastic.eastInOut}, 0.7);
 }
 
 // 2. color is chosen
-// find num in hex and apply that to card spins + result
-
-
-
-
-function findColorNum(color) {
+function findColorNum(color, div) {
+	// animate elasticity on button when clicked
+	TweenMax.from(div, 0.5, {opacity:0, scale:0, ease: Bounce.easeInOut});
 
 	console.log("COLOR IS " + color)
+	// find num in hex and apply that to card spins + result
 	var colorNum;
 	// iterate thru hex chars
 	for (var m = 1; m < color.length; m++) {
@@ -93,9 +99,10 @@ function findColorNum(color) {
 		colorNum = Math.floor(Math.random()*6);
 	}
 	flipCard(colorNum, color);
+
 }
 
-
+// spin card when revealing fortune
 function flipCard(num, color) {
 	TweenMax.set(".board", {transformPerspective: 600});
 	TweenMax.staggerFrom(".cards", 1.5, {
@@ -112,49 +119,106 @@ function flipCard(num, color) {
 	tellFortune(num, color);
 }
 
+// replace card with fortune word & pic
 function tellFortune(num, color) {
 	for (var n =0; n < 4; n++) {
 		var cards = document.getElementsByClassName("cards");
-		if (cards[n].id != "assigned") {
+		if (cards[n].id != "assigned" && cards[n].id != "lr") {
 			var card = cards[n];
 			card.style.border = "6px solid " + color;
 			card.style.color = color;
-			card.style.textShadow = "2px 2px 5px #000000";
-
+			card.style.textShadow = "2px 2px 4px #000000";
 			card.innerHTML = fortunes(); 
-			card.style.backgroundImage = fortuneImages(num);
+			card.style.backgroundImage = fortuneImages();
 			card.style.backgroundSize = "150%";
 			card.setAttribute("class", "assigned");
+			round += 1;
 			break;
 		}
 	}
+			if (round === 2) {
+				round2();
+			}
+			else {
+				round3();
+			}
 }
 
-
-
-
+// affix a fortune word to a card
 function fortunes() {
-
 	var num = Math.floor(Math.random()*cards.length);
 	var future = cards[num];
 	cards.splice(num, 1);
-	console.log(future);
-	console.log(cards);
 	return future;
 }
-
-
-function fortuneImages(num) {
-	var num = Math.floor(Math.random()*num);
+//affix background image to fortune card
+function fortuneImages() {
+	var num = Math.floor(Math.random()*imgs.length);
 	var img = imgs[num];
 	imgs.splice(num, 1);
 	return "url('imgs/" + img + ".png')";
 }
+// 3. choose a num, num flips the next fortune
+function round2() {
+
+	var menu = document.getElementById("menu");
+	var colorDivs = menu.getElementsByTagName("div");
+	for (var p = 0; p < colorDivs.length; p++) {
+		colorDivs[p].style.backgroundColor = "black";
+		colorDivs[p].innerHTML = randNum();
+		colorDivs[p].style.color = assignColor();
+		colorDivs[p].style.borderRadius = "50%";
+		colorDivs[p].style.fontSize = "1.25em";
+	}
+	// change button text
+	document.getElementById("begin").value = "I want different numbers";
+	// slide in new header
+    var header = document.getElementById("header");
+	header.innerHTML = "Choose A Number";
+	// slide in title from right
+	TweenMax.from(header, 2, {opacity:0, x:800, ease:Elastic.eastInOut}, 0.7);
+}
+
+function round3() {
+	var menu = document.getElementById("menu");
+	var colorDivs = menu.getElementsByTagName("div");
+	for (var q = 0; q < colorDivs.length; q++) {
+		colorDivs[q].style.backgroundImage = "imgs/cardLowRes.png";
+		colorDivs[q].innerHTML = "";
+		colorDivs[q].style.border = "1px solid grey";
+		TweenMax.to(colorDivs[q], .75, {
+ 		   		borderRadius:randShape()
+			});
+	}
+	// change button text
+	document.getElementById("begin").value = "I want new shapes";
+	// slide in new header
+    var header = document.getElementById("header");
+	header.innerHTML = "Choose A Shape";
+	// slide in title from right
+	TweenMax.from(header, 2, {opacity:0, x:800, ease:Elastic.eastInOut}, 0.7);
+	round4();
+}
+
+function round4() {
+	document.getElementById("begin").value = "I want new shapes";
+
+}
+
+function randShape() {
+	var shapes = ["25px","50px","0px 20px","20px 0px","0px 20px 50px","20px 0px 50px","0px 20px 50px 50px","50px 0px 20px 50px","50px 50px 50px 0px","50px 0px 50px 50px"];
+	var shapeIndex = Math.floor(Math.random()*shapes.length)
+	var shapeMaker = shapes[shapeIndex];
+	return shapeMaker
+}
 
 
-
-
-// 3. choose a num
-// apply num to card spins + result
+function randNum() {
+  var rand = Math.floor(Math.random()*200);
+  return rand;
+}
 
 begin();
+}
+
+fortuneTellerApp();
